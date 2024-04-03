036e966625899f80c49c08d1f3345dddc68e58b4
Eslem Karakaş <52754729+eslemkarakas@users.noreply.github.com>
2024-04-03 18:32:01 +0300

    feat: Adds new measurement functions into metric.py

--- utils/metrics/measure.py
+++ utils/metrics/measure.py
@@ -1,24 +1,24 @@
-# import standard packages
+# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 from sklearn.metrics import f1_score
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
- 
+
 def mse(y_true, y_pred):
-        result = mean_squared_error(y_true, y_pred)
-        return result
+    result = mean_squared_error(y_true, y_pred)
+    return result
     
 def mae(y_true, y_pred):
-        result = mean_absolute_error(y_true, y_pred)
-        return result
+    result = mean_absolute_error(y_true, y_pred)
+    return result
     
 def mape(y_true, y_pred):
-        result = mean_absolute_percentage_error(y_true, y_pred)
-        return result
+    result = mean_absolute_percentage_error(y_true, y_pred)
+    return result
     
 def rmse(y_true, y_pred):
-        result = np.sqrt(mean_squared_error(y_true, y_pred))
-        return result
+    result = np.sqrt(mean_squared_error(y_true, y_pred))
+    return result
 
 def f1(y_true, y_pred, optimization=False):
     if isinstance(y_true, pd.DataFrame):
@@ -48,6 +48,14 @@ class Metrics:
 
         if self.metric == 'f1':
             self.metric = f1
+        elif self.metric == 'mae':
+            self.metric = mae
+        elif self.metric == 'rmse':
+            self.metric = rmse
+        elif self.metric == 'mape':
+            self.metric = mape
+        else:
+            pass
             
     def __call__(self, y_true, y_pred):
         return self.metric(y_true, y_pred)
